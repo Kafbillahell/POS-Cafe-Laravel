33d88a79d0224b17e5fa63065b5618617b780472
kafbillah <kafbillahelhusni@gmail.com>
2025-04-23 08:47:09 +0700

    laravel dasboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class, 'index']);
